c19b044a7fe5622ae60323c1b118866ac22d8ad2
MarcoBalo <marco.balossini98@gmail.com>
2022-01-09 12:57:24 +0100

    Feat: Project creation, few basic functions

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='gdbPy',
+    packages=find_packages(include=['gdbPy']),
+    version='0.1.0',
+    description='gdb scripts in Python',
+    author='Marco Balossini',
+    license='MIT',
+)
